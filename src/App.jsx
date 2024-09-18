10a977d982f80ab83d49d6503deea9c445e0afa5
Rakibulislam-emon <rakibulislamemon60>
2024-09-18 22:07:44 +0600

    pull staging

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-
 import './App.css'
 
 function App() {
-
-// test nahiyan
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello good peoples!
-    </h1>
-    </>
-  )
+ <>
+
+ </>
+  );
 }
 
-export default App
+export default App;
