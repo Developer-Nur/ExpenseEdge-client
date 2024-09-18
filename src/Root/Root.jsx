1a4fe14a80984c94e2b9e70191246ae2518ada5f
mahade <mahade.adib45@gmail.com>
2024-09-18 21:18:42 +0600

    folder shifted

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar/Navbar';
-import Reports from '../Components/Reports/Reports';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Reports></Reports>
+            <Outlet></Outlet>         
         </div>
     );
 };
