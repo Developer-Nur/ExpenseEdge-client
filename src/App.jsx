2274d3febfc66086c83677a02f1d7fe058f10b80
Developer Nur <nursweb6@gmail.com>
2024-09-18 15:02:34 +0600

    App.jsx clearing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 
 import './App.css'
-import Banner from './components/Banner'
 
 function App() {
 
-// test nahiyan
+
   return (
     <>
-    <div className='max-w-screen-2xl mx-auto'>
-
-    <Banner></Banner>
-    </div>
     </>
   )
 }
