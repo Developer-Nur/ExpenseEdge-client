64050afd83d980d5c042e07c912d8ddb439df8ac
Developer Nur <nursweb6@gmail.com>
2024-09-19 00:07:39 +0600

    Duplicate Layout File deleted

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../Components/Navbar/Navbar';
+import NavBar from '../Shared/Header/NavBar';
+// import Navbar from '../Components/Header/Navbar';
 
 const Root = () => {
     return (
         <div>
-            <Navbar></Navbar>
+            <NavBar></NavBar>
             <Outlet></Outlet>
         </div>
     );
