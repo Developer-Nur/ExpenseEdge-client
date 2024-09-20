9ace6bf078343de6efabff52dae4dee45ebe7f8c
Developer Nur <nursweb6@gmail.com>
2024-09-20 12:39:11 +0600

    sometnig

---
+++ src/Pages/Singup/Singup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Singup = () => {
+    return (
+        <div className='w-3/5 mx-auto text-center mt-20 font-semibold'>
+            Sing up page - under Construction
+        </div>
+    );
+};
+
+export default Singup;
