056e3bfcdd04dc1daa4be55b759c5f0fd0825d6c
Sangma83 <poushalisangma13@gmail.com>
2024-09-20 12:22:46 +0600

    completed footer and testimonial section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Reports from '../../Components/Reports/Reports';
+// import Footer from '../../shared/Footer/Footer';
+import Testimonial from '../../Components/Testimonial/Testimonial';
 
 const Home = () => {
     return (
         <div>
             Home
             <Reports></Reports>
+            <Testimonial></Testimonial>
+            {/* <Footer></Footer> */}
         </div>
     );
 };
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../Shared/Header/NavBar';
-// import Navbar from '../Components/Header/Navbar';
+import Footer from '../Shared/Footer/Footer';
+
 
 const Root = () => {
     return (
         <div>
-<<<<<<< HEAD
             <NavBar></NavBar>
             <Outlet></Outlet>
-=======
-            <Navbar></Navbar>
-            <Outlet></Outlet>         
->>>>>>> 010b81ab40b1a507462edf498f6e33a076727ca7
+            <Footer></Footer>
         </div>
     );
 };
